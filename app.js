b0bb8802d9e4c079543aa0b7319627b65fcac4fa
Yug34 <vertilax1337@gmail.com>
2021-05-08 02:23:16 +0530

    Cleaned up app.js

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
 //DEBUG=localLibrary:* npm start
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let wikiRouter = require('./routes/wiki');
-let catalogRouter = require('./routes/catalog');
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
+let wikiRouter = require("./routes/wiki");
+let catalogRouter = require("./routes/catalog");
 
 let app = express();
 
-let mongoose = require('mongoose');
-let mongoDB = 'mongodb+srv://verti:yug123@cluster0.yisxz.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+let mongoose = require("mongoose");
+let mongoDB =
+  "mongodb+srv://verti:yug123@cluster0.yisxz.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/wiki', wikiRouter);
-app.use('/catalog', catalogRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/wiki", wikiRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
